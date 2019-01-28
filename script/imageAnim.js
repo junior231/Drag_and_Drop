343b2ca84cf00b4b8e7b52a94bb09b843271ae05
Ilo <c_ilo@tss-hr420-dm23.local>
2019-01-28 16:25:00 -0500

    got the javascript working fine

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,18 +1,42 @@
 (() => {
-	// select the elements that you want to work with
-	let theButton = document.querySelector("#buttonHolder img");
-	// window.addEventListener("load", changeHeaderText);
+	// variables always come first 
+	// set up the puzzle pieces and boards
+	
+
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+
+	let piecesBoard = document.querySelector(".puzzle-pieces"),
+		puzzleBoard = document.querySelector(".puzzle-board"),
+		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
 
 	// functions go in the middle
-	function changeHeaderText(){
-		document.querySelector("h1").textContent = "Changing with JS"
+	function createPuzzlePieces(pictureIndex) {
+		// generate puzzle pieces for left and right side
+		// debugger;
+		pieces.forEach((piece, index) => {
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+
+			piecesBoard.innerHTML += newPuzzlePiece;
+
+		});
+
+		puzzleBoard.style.backgroundImage = `url(./images/background${pictureIndex}.jpg)`;
+	} 
+
+
+
+	function resetPuzzlePieces() {
+		// empty the thumbnail container
+		piecesBoard.innerHTML = "";
+		createPuzzlePieces(this.dataset.puzzleref);
 	}
 
+	// event handling down here
+	puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
 
-	// event handling goes at the bottom
-
-	theButton.addEventListener("click", changeHeaderText);
-	// set up the puzzles and pieces and boards
+	createPuzzlePieces(0);
+	
 	
 	
 })();
