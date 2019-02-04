9c297fa14b06d08e6324d24a940bb7a43652d556
Ilo <c_ilo@tss-hr420-dm24.local>
2019-02-04 16:38:17 -0500

    drag and drop working partially

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -9,22 +9,61 @@
 		puzzleBoard = document.querySelector(".puzzle-board"),
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+	let dropZones = document.querySelectorAll('.drop-zone');
+
 
 	// functions go in the middle
 	function createPuzzlePieces(pictureIndex) {
 		// generate puzzle pieces for left and right side
 		// debugger;
 		pieces.forEach((piece, index) => {
-			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+			let newPuzzlePiece = `<img draggable id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
 
 			piecesBoard.innerHTML += newPuzzlePiece;
 
 		});
 
 		puzzleBoard.style.backgroundImage = `url(./images/background${pictureIndex}.jpg)`;
+
+		initDrag();
 	} 
 
 
+	// drag and drop functionality goes here
+	
+
+	function initDrag() {
+		piecesBoard.querySelectorAll('img').forEach(img => {
+			img.addEventListener("dragstart", function(e) {
+				console.log('draggin...');
+
+
+				e.dataTransfer.setData("text/plain", this.id);
+			});
+		});
+	}
+
+
+
+	// handle dragover and drop
+	
+	dropZones.forEach(zone =>{
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log("you dragged over me");
+		});
+	})
+
+
+	dropZones.forEach(zone =>{
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log("you dropped something on me");
+
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	})
 
 	function resetPuzzlePieces() {
 		// empty the thumbnail container
