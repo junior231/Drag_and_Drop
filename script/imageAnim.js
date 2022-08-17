b929235cded1636544c4dc9e8798d162af359c28
junior231 <ilocollins231@gmail.com>
2022-08-16 21:14:15 -0400

    update script.

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,87 +1,73 @@
 (() => {
-	// variables always come first 
-	// set up the puzzle pieces and boards
-	
+  // set up the puzzle pieces and boards
+  const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+  let piecesBoard = document.querySelector(".puzzle-pieces"),
+    puzzleBoard = document.querySelector(".puzzle-board"),
+    puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
-	let piecesBoard = document.querySelector(".puzzle-pieces"),
-		puzzleBoard = document.querySelector(".puzzle-board"),
-		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+  let dropZones = document.querySelectorAll(".drop-zone");
 
-	let dropZones = document.querySelectorAll('.drop-zone');
+  // functions go in the middle
+  function createPuzzlePieces(pictureIndex) {
+    // generate puzzle pieces for left and right side
 
+    pieces.forEach((piece, index) => {
+      let newPuzzlePiece = `<img draggable id="piece${index}" class="puzzle-image" src="images/${
+        piece + pictureIndex
+      }.jpg" alt="thumbnail">`;
 
-	// functions go in the middle
-	function createPuzzlePieces(pictureIndex) {
-		// generate puzzle pieces for left and right side
-		// debugger;
-		pieces.forEach((piece, index) => {
-			let newPuzzlePiece = `<img draggable id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+      piecesBoard.innerHTML += newPuzzlePiece;
+    });
 
-			piecesBoard.innerHTML += newPuzzlePiece;
+    puzzleBoard.style.backgroundImage = `url(./images/background${pictureIndex}.jpg)`;
 
-		});
+    initDrag();
+  }
 
-		puzzleBoard.style.backgroundImage = `url(./images/background${pictureIndex}.jpg)`;
+  // drag and drop functionality goes here
 
-		initDrag();
-	} 
+  function initDrag() {
+    piecesBoard.querySelectorAll("img").forEach((img) => {
+      img.addEventListener("dragstart", function (e) {
+        e.dataTransfer.setData("text/plain", this.id);
+      });
+    });
+  }
 
+  // handle dragover and drop
 
-	// drag and drop functionality goes here
-	
+  dropZones.forEach((zone) => {
+    zone.addEventListener("dragover", function (e) {
+      e.preventDefault();
+      console.log("you dragged over me");
+    });
+  });
 
-	function initDrag() {
-		piecesBoard.querySelectorAll('img').forEach(img => {
-			img.addEventListener("dragstart", function(e) {
-				console.log('draggin...');
+  dropZones.forEach((zone) => {
+    zone.addEventListener("drop", function (e) {
+      let zoneContent = zone.innerHTML;
+      if (!zoneContent) {
+        let piece = e.dataTransfer.getData("text/plain");
+        e.target.appendChild(document.querySelector(`#${piece}`));
+        console.log("you dropped something on me");
+      } else {
+        e.preventDefault();
+      }
+    });
+  });
 
+  function resetPuzzlePieces() {
+    // empty the thumbnail container
+    piecesBoard.innerHTML = "";
+    dropZones.forEach((zone) => (zone.innerHTML = ""));
+    createPuzzlePieces(this.dataset.puzzleref);
+  }
 
-				e.dataTransfer.setData("text/plain", this.id);
-			});
-		});
-	}
+  // event handling down here
+  puzzleSelectors.forEach((puzzle) =>
+    puzzle.addEventListener("click", resetPuzzlePieces)
+  );
 
-
-
-	// handle dragover and drop
-	
-	dropZones.forEach(zone =>{
-		zone.addEventListener("dragover", function(e) {
-			e.preventDefault();
-			console.log("you dragged over me");
-		});
-	})
-
-
-	dropZones.forEach(zone =>{
-        zone.addEventListener("drop", function(e) {
-            let zoneContent = zone.innerHTML;
-            if (!zoneContent) {
-                let piece = e.dataTransfer.getData("text/plain");
-                e.target.appendChild(document.querySelector(`#${piece}`));
-                console.log("you dropped something on me");
-            }
-            else {
-                e.preventDefault();
-            }
-        });
-    })
-
-	function resetPuzzlePieces() {
-		// empty the thumbnail container
-		piecesBoard.innerHTML = "";
-		dropZones.forEach(zone => zone.innerHTML = "");
-		createPuzzlePieces(this.dataset.puzzleref);
-	}
-
-	// event handling down here
-	puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
-
-	createPuzzlePieces(0);
-	
-	
-	
+  createPuzzlePieces(0);
 })();
-
